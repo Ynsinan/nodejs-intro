84e775aa28dd0abcfebbda7018a5791f009f6bc0
Yunus İNAN <yns.inan1660@gmail.com>
2021-12-24 12:56:57 +0300

    Odev5 ve Web Sunucusu yazma bölümleri bitti

---
+++ NodeJsTemelleri/Odevler/Odev5/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>İNDEX SAYFASINA HOŞGELDİNİZ");
+  } else if (url === "/hakkimda") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>HAKKIMDA SAYFASINA HOŞGELDİNİZ");
+  } else if (url === "iletisim") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>İLETİŞİM SAYFASINA HOŞGELDİNİZ");
+  }
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı`);
+});
